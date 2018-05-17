31786599acf523b1aa5ed7fed3a2d6addc00da91
Rakhee Bhojakar <rakheebhojakar@gmail.com>
2018-05-17 01:25:32 -0700

    Update iris_keras_impl.py

--- iris_keras_impl.py
+++ iris_keras_impl.py
@@ -1,39 +1,133 @@
-#Import necessary packages
+from numpy import exp, array, random, dot
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.preprocessing import LabelBinarizer
-from keras.models import Sequential #Sequential Models
-from keras.layers import Dense #Dense Fully Connected Layer Type
-from keras.optimizers import SGD #Stochastic Gradient Descent Optimizer
 
-#load the iris dataset
-iris = load_iris()
-#inputs contain 4 features
-X = iris.data[:, 0:4]
-#labels
-y = iris.target
-#print the distinct y labels
-print(np.unique(y))
+class NeuralNetwork():
+    def __init__(self):
+        random.seed(1)
 
-#One Hot Encode Y
-encoder = LabelBinarizer()
-Y = encoder.fit_transform(y)
-
-def baseline_network():
-    model = Sequential()
-    model.add(Dense(3, input_shape=(4,), activation='sigmoid'))
-    model.add(Dense(3, activation='sigmoid'))
-    model.add(Dense(3, activation='softmax'))
+        # Number of nodes in hidden layers
+        l2 = 3     # layer 2
+        l3 = 3     # Layer 3
         
-    #stochastic gradient descent
-    sgd = SGD(lr=0.001, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-    return model
+        # Learning rate
+        self.eta = 0.1
 
-neural_network = baseline_network()
-neural_network.fit(X,Y, epochs=500, batch_size=1)
+        # assign random weights
+        self.weights1 = np.random.uniform(low = -0.05, high= +0.05, size= ((4, l2)))
+        self.weights2 = np.random.uniform(low = -0.05, high= +0.05, size= ((l2,l3))) 
+        self.weights3 = np.random.uniform(low = -0.05, high= +0.05, size= ((l3, 3)))
 
-np.set_printoptions(suppress=True)
-prediction = neural_network.predict(X[0:10], batch_size=32, verbose=0)
-print("Prediction \n" + str(prediction))
-print("Target \n" + str(Y[0:10]))
+    def __sigmoid(self, x):
+        return 1/(1+exp(-x))
+
+    # derivative of sigmoid function
+    def __sigmoid_derivative(self, x):
+        return x*(1-x)
+
+    # train neural network
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        correct = 0
+        total = 0
+        for epoch in range(number_of_training_iterations):     # StochasticGD implementation
+            for samples in range(training_set_inputs.shape[0]):
+                
+                training_set_inputs_ = training_set_inputs[samples:samples+1]
+                training_set_outputs_ = training_set_outputs[samples:samples+1]
+
+                a2 = self.__sigmoid(dot(training_set_inputs_, self.weights1))
+                a3 = self.__sigmoid(dot(a2, self.weights2))
+                output = self.__sigmoid(dot(a3, self.weights3))
+
+                # calculate 'error'
+                del4 = (training_set_outputs_ - output)*self.__sigmoid_derivative(output)
+
+                # find 'errors' in each layer
+                del3 = dot(self.weights3, del4.T)*(self.__sigmoid_derivative(a3).T)
+                del2 = dot(self.weights2, del3)*(self.__sigmoid_derivative(a2).T)
+
+                # get gradients for each layer
+                gradient3 = dot(a3.T, del4)
+                gradient2 = dot(a2.T, del3.T)
+                gradient1 = dot(training_set_inputs_.T, del2.T)
+
+                # adjust weights accordingly
+                self.weights1 += self.eta * gradient1
+                self.weights2 += self.eta * gradient2
+                self.weights3 += self.eta * gradient3
+                
+            for i in range(training_set_inputs.shape[0]):
+        
+                output = neural_network.forward_pass(training_set_inputs[i])
+                prediction = np.zeros(output.shape)
+                prediction[np.argmax(output)] = 1
+                if (np.argmax(labels_[i]) == np.argmax(prediction)):
+                    correct+=1
+                else:
+                    correct+=0
+                total += 1
+
+            accuracy = correct / total
+            print("epoch: ", epoch, ", accuracy: ", accuracy)
+
+    def forward_pass(self, inputs):
+        # pass inputs through neural network
+        a2 = self.__sigmoid(dot(inputs, self.weights1))
+        a3 = self.__sigmoid(dot(a2, self.weights2))
+        output = self.__sigmoid(dot(a3, self.weights3)) 
+        return output
+
+if __name__ == "__main__":
+    rnd_ind = random.permutation(150)
+    #load the iris dataset
+    iris = load_iris()
+    #inputs contain 4 features
+    X = iris.data[:, 0:4]
+    #preprocessing of input
+    X = X/np.max(X)
+    #labels
+    y = iris.target
+   
+    '''
+    #One Hot Encode Y
+    encoder = LabelBinarizer()
+    Y = encoder.fit_transform(y)
+    '''    
+    y_ = y.reshape((len(x), 1))
+    enc = OneHotEncoder()
+    enc.fit(y_)
+    labels_ = enc.transform(y_).toarray()   # one hot encoding of y
+    
+    neural_network = NeuralNetwork()
+
+    print ("Random starting weights (layer 1): ")
+    print (neural_network.weights1)
+    print ("\nRandom starting weights (layer 2): ")
+    print (neural_network.weights2)
+    print ("\nRandom starting weights (layer 3): ")
+    print (neural_network.weights3)
+
+    # the training set   
+    training_set_inputs = X
+    training_set_outputs = labels_
+
+    neural_network.train(training_set_inputs, training_set_outputs, 2000)
+
+    print ("\nNew weights (layer 1) after training: ")
+    print (neural_network.weights1)
+    print ("\nNew weights (layer 2) after training: ")
+    print (neural_network.weights2)
+    print ("\nNew weights (layer 3) after training: ")
+    print (neural_network.weights3)
+
+
+    '''
+    temp = np.zeros(3)
+    for i in range(output.shape[0]):
+        temp[np.argmax(output[i,:])] = 1
+        prediction[i] = temp
+       
+    '''
+  
+        
+    
